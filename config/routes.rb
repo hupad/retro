bd35aba8f4cd3436af58640380b4cbb8c8e66cc8
sarvin <scoachbuilder@dealersocket.com>
2016-01-07 14:12:33 -0600

    routes for api, user controller added under api v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,19 @@ Retro::Application.routes.draw do
 
   # List all retros of a team
   resources :sretros do 
-	get "retro_notes", to: "retro_notes#index"
-	get 'retro_notes/new', to: "retro_notes#new", as: :new_retro_notes
-	post 'retro_notes/create', to: "retro_notes#create", as: :create_retro_notes
+    get "retro_notes", to: "retro_notes#index"
+    get 'retro_notes/new', to: "retro_notes#new", as: :new_retro_notes
+    post 'retro_notes/create', to: "retro_notes#create", as: :create_retro_notes
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :update, :delete, :show]
+      resources :teams, only: [:index]
+
+      resources :sretros do
+        post 'retro_notes/create', to: "retro_notes#create", as: :create_retro_notes
+      end
+    end
+  end
 end
