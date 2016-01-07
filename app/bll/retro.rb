da278e655dd7ce2b81034593bc3a00ee256b912c
harish <hupadhyayula@dealersocket.com>
2016-01-07 16:04:04 -0600

    deleted sretros bll

---
+++ app/bll/retro.rb
@@ -0,0 +1,3 @@
+class Retro
+
+end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,18 @@
 class NotesController < ApplicationController
+	def new
+		@note = Note.new
+		@sretro = Sretro.find(params[:sretro_id])
+	end
+
+
+	def create
+		@note = @note.create(notes_params)
+		redirect_to @note
+	end
+
+	private
+
+	def notes_params
+		params.require(:note).permit(:sretro_id, :what_went_well, :what_did_not_go_well, :improvement_areas)
+	end
 end
