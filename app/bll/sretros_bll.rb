75ea8e492d3d443bc2422eff3c11445c1795b8ce
harish <hupadhyayula@dealersocket.com>
2016-01-07 15:59:03 -0600

    deleted notes_controller_controller

--- app/bll/sretros_bll.rb
+++ app/bll/sretros_bll.rb
@@ -1,4 +1,6 @@
 class SretroBll
 
-
+	def list_all_retros
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Retro::Application.routes.draw do
   
   devise_for :users
   root 'home#about'
-  
+
   resources :sretros do 
 	 resources :notes
     # get "retro_notes", to: "retro_notes#index"
-    # get 'retro_notes/new', to: "retro_notes#new", as: :new_retro_notes
+    # get 'retro_notes', to: "retro_notes#new", as: :new_retro_notes
     # post 'retro_notes/create', to: "retro_notes#create", as: :create_retro_notes
   end
 
