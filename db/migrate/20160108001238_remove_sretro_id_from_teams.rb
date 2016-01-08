5865bb4e166fc54e8c4df6a7d5ea34a3ef3bee64
sarvin <scoachbuilder@dealersocket.com>
2016-01-08 09:58:42 -0600

    proper closing of method and class

--- db/migrate/20160108001238_remove_sretro_id_from_teams.rb
+++ db/migrate/20160108001238_remove_sretro_id_from_teams.rb
@@ -1,3 +1,5 @@
 class RemoveSretroIdFromTeams < ActiveRecord::Migration
   def change
-  	remove_reference :teams, :sretro, index: true, foreign_key: trueend
+    remove_reference :teams, :sretro, index: true, foreign_key: true
+  end
+end
